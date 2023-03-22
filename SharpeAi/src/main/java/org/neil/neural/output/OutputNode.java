d488fe9e8f019cc23f9829bab345dfe1ff77eca4
neil <neil@nope.com>
2023-03-22 01:43:28 -0700

    refactor: move coordinateMap Creature and Inputs into MoveLeftSimulation

--- SharpeAi/src/main/java/org/neil/neural/output/OutputNode.java
+++ SharpeAi/src/main/java/org/neil/neural/output/OutputNode.java
@@ -1,11 +1,10 @@
 package org.neil.neural.output;
 
 import org.neil.neural.Node;
-import org.neil.object.Creature;
 
 public interface OutputNode<E> extends Node {
 
-    void consumeOutput(E creatureConsumer);
+    void consumeOutput(E e);
 
     OutputNode copy();
 }
--- SharpeAi/src/test/java/org/neil/neural/OutputNodeTest.java
+++ SharpeAi/src/test/java/org/neil/neural/OutputNodeTest.java
@@ -3,7 +3,6 @@ package org.neil.neural;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.neil.neural.output.OutputNode;
-import org.neil.object.Creature;
 
 public class OutputNodeTest {
 
@@ -17,13 +16,13 @@ public class OutputNodeTest {
         Assertions.assertEquals(0, toTest.getStored());
     }
 
-    private static class OutputNodeNodeTester extends NodeDefault implements OutputNode<Creature> {
+    private static class OutputNodeNodeTester extends NodeDefault implements OutputNode<Integer> {
         public OutputNodeNodeTester() {
             super(1, 100);
         }
 
         @Override
-        public void consumeOutput(Creature creature) {
+        public void consumeOutput(Integer creature) {
             clearStorage();
         }
 
